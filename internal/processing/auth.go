693ec9e064d533e2b2aa79581ee17fe016a7d28a
Alex Forsythe <awforsythe@gmail.com>
2024-02-13 17:38:49 -0500

    Debit points etc. for all users, not just a single test user

--- internal/processing/auth.go
+++ internal/processing/auth.go
@@ -2,7 +2,6 @@ package processing
 
 import (
 	"context"
-	"fmt"
 	"strings"
 
 	"github.com/golden-vcr/auth"
@@ -10,11 +9,6 @@ import (
 )
 
 func requestServiceToken(ctx context.Context, client auth.ServiceClient, viewer *core.Viewer) (string, error) {
-	// TEMP: Only allow this service to update the state of a single test user for now
-	if viewer.TwitchUserId != "90790024" {
-		return "", fmt.Errorf("testing is restricted to events instigated by user 90790024")
-	}
-
 	return client.RequestServiceToken(ctx, auth.ServiceTokenRequest{
 		Service: "dispatch",
 		User: auth.UserDetails{
