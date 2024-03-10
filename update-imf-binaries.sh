f9b4c3a533cec57f3b5b11f947a1b7c606e9badf
Alex Forsythe <awforsythe@gmail.com>
2024-03-10 18:15:06 -0400

    Add script for fetching latest imf binaries

---
+++ update-imf-binaries.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+
+IMAGE_FILTERS_RELEASE=v0.1.2
+
+LINUX_ZIP_FILENAME="imf_${IMAGE_FILTERS_RELEASE}_linux.zip"
+WIN_ZIP_FILENAME="imf_${IMAGE_FILTERS_RELEASE}_win.zip"
+
+LINUX_ZIP_URL="https://github.com/golden-vcr/image-filters/releases/download/${IMAGE_FILTERS_RELEASE}/${LINUX_ZIP_FILENAME}"
+WIN_ZIP_URL="https://github.com/golden-vcr/image-filters/releases/download/${IMAGE_FILTERS_RELEASE}/${WIN_ZIP_FILENAME}"
+
+mkdir -p external/bin
+curl -L -o "external/bin/$LINUX_ZIP_FILENAME" "$LINUX_ZIP_URL"
+curl -L -o "external/bin/$WIN_ZIP_FILENAME" "$WIN_ZIP_URL"
+(cd external/bin && unzip "$LINUX_ZIP_FILENAME" && rm "$LINUX_ZIP_FILENAME")
+(cd external/bin && unzip "$WIN_ZIP_FILENAME" && rm "$WIN_ZIP_FILENAME")
